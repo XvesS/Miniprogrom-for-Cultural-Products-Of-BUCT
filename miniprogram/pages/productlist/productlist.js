d7c969b841453d3b58aebf0fd7df169eada0c5d9
xueluming <83113926@qq.com>
2021-04-09 20:44:53 +0800

    gengyue

--- miniprogram/pages/productlist/productlist.js
+++ miniprogram/pages/productlist/productlist.js
@@ -1,55 +1,137 @@
 // pages/productlist/productlist.js
-var order_detail = {
-  "data": [
+Page({
+    data: {
+        moveParams: {
+          scrollLeft: 0
+        },//固定
+        cateItems: [
     {
-      "buyNums": 1,
-      "orderList": [
+      cate_id:1,
+      abbba:'待付款',
+      children:[
         {
-          "buyNum": 2,
-          "goodsImg": "../../images/cart.jpg",
-          "goodsName": "列表商品",
-          "goodsStandardDes": "颜色：红色  尺码：s",
-          "sellPrice": 25,
-          "shopId": 1,
-        }
-      ],
-      "orderNum": "abc2511483687801946",
-      "orderStatusStr": "待付款",
-      "orderTime": 1483687801000,
-      "totalPrice":50
+          buyNum: 2,
+          goodsImg: "../../images/cart.jpg",
+          goodsName: "列表商品",
+          goodsStandardDes: "颜色：红色  尺码：s",
+          sellPrice: 25,
+          shopId: 1,
+          orderNum: "abc2511483687801946",
+          orderTime: "1483687801000",
+          totalPrice:50
     },
     {
-      "buyNums": 1,
-      "orderList": [
-        {
-          "buyNum": 1,
-          "goodsImg": "../../images/cart.jpg",
-          "goodsName": "列表商品",
-          "goodsStandardDes": "颜色：红色  尺码：s",
-          "sellPrice": 19.9,
-          "shopId": 1,
-        }
-      ],
-      "orderNum": "abc2511483616883663",
-      "orderStatusStr": "已完成",
-      "orderTime": 1483616883000,
-      "totalPrice": 19.9,
+          buyNum: 1,
+          goodsImg: '../../images/cart.jpg',
+          goodsName: '列表商品',
+          goodsStandardDes:" 颜色：红色  尺码：s",
+          sellPrice: 19.9,
+          shopId: 1,
+          orderNum: "abc2511483616883663",
+          orderTime: "1483616883000",
+          totalPrice: "19.9",
     }
-  ],
+      ],
+
+
+    },
+    {
+      cate_id:1,
+      abbba:'待发货',
+      children:[
+        {
+          buyNum: 2,
+          goodsImg: "../../images/cart.jpg",
+          goodsName: "列表商品1",
+          goodsStandardDes: "颜色：红色  尺码：s",
+          sellPrice: 25,
+          shopId: 1,
+          orderNum: "abc2511483687801946",
+          orderTime: "1483687801000",
+          totalPrice:50
+    },
+    {
+          buyNum: 1,
+          goodsImg: '../../images/cart.jpg',
+          goodsName: '列表商品2',
+          goodsStandardDes:" 颜色：红色  尺码：s",
+          sellPrice: 19.9,
+          shopId: 1,
+          orderNum: "abc2511483616883663",
+          orderTime: "1483616883000",
+          totalPrice: "19.9",
+    }
+      ],
+
+
+    },
+    {
+      cate_id:1,
+      abbba:'待收货',
+      children:[
+        {
+          buyNum: 2,
+          goodsImg: "../../images/cart.jpg",
+          goodsName: "列表商品3",
+          goodsStandardDes: "颜色：红色  尺码：s",
+          sellPrice: 25,
+          shopId: 1,
+          orderNum: "abc2511483687801946",
+          orderTime: "1483687801000",
+          totalPrice:50
+    },
+    {
+          buyNum: 1,
+          goodsImg: '../../images/cart.jpg',
+          goodsName: '列表商品4',
+          goodsStandardDes:" 颜色：红色  尺码：s",
+          sellPrice: 19.9,
+          shopId: 1,
+          orderNum: "abc2511483616883663",
+          orderTime: "1483616883000",
+          totalPrice: "19.9",
+    }
+      ],
+
+
+    },
+    {
+      cate_id:1,
+      abbba:'已完成',
+      children:[
+        {
+          buyNum: 2,
+          goodsImg: "../../images/cart.jpg",
+          goodsName: "列表商品5",
+          goodsStandardDes: "颜色：红色  尺码：s",
+          sellPrice: 25,
+          shopId: 1,
+          orderNum: "abc2511483687801946",
+          orderTime: "1483687801000",
+          totalPrice:50
+    },
+    {
+          buyNum: 1,
+          goodsImg: '../../images/cart.jpg',
+          goodsName: '列表商品6',
+          goodsStandardDes:" 颜色：红色  尺码：s",
+          sellPrice: 19.9,
+          shopId: 1,
+          orderNum: "abc2511483616883663",
+          orderTime: "1483616883000",
+          totalPrice: "19.9",
+    }
+      ],
+
+
+    }
+    
+    
+ 
+ ]
 }
 
-Page({
-  data: {
-    curNav: "0",
-    list: ["待付款","待发货","待收货","已完成"]
-  },
-  switchTab: function(e) {
-  },
-  onLoad: function() {
-    this.setData({
-      curNav: 0,
-    });
-  },
+ ,
   onShow: function() {
     var self = this;
     var self  = self,
@@ -61,10 +143,13 @@ Page({
       order: info
     });
   },
-  getDetail: function(e) {
 
-  },
-  goPay: function() {
 
-  },
+switchRightTab:function(e){
+let id = e.target.dataset.cate_id,index=e.target.dataset.index;
+this.setData({
+  curNav:id,
+  curIndex:index
+})
+},
 })
